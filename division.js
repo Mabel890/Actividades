ffb910921505c874bff3d45b6ff02a0e736562d2
Mabel <mabelvillada890@gmail.com>
2021-04-09 22:33:37 -0500

    Division

---
+++ division.js
@@ -0,0 +1,8 @@
+const variable1 = Number(prompt('Ingrese una variable: '));
+const variable2 = Number(prompt('Ingrese una variable: '));
+
+function division(variable1, variable2) {
+    let division = variable1 / variable2;
+    console.log(division);
+};
+division(variable1, variable2);
