e3b82a642370a6c84ebc68a93242997a4f10d2fb
Mabel <mabelvillada890@gmail.com>
2021-04-09 23:05:53 -0500

    Limpiando la rama

--- suma.js
+++
@@ -1,8 +0,0 @@
-const variable1 = Number(prompt('Ingrese una variable: '));
-const variable2 = Number(prompt('Ingrese una variable: '));
-
-function suma(variable1, variable2) {
-    let suma = variable1 + variable2;
-    console.log(suma);
-};
-suma(variable1, variable2);
