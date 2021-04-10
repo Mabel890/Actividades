e83cdc365dd20d7d94057dbb319c65f3dc854a7c
Mabel <mabelvillada890@gmail.com>
2021-04-09 20:08:57 -0500

    Suma

--- operaciones.js
+++ operaciones.js
@@ -1,5 +1,8 @@
-const variable1 = Number(prompt("Ingrese una variable: ", ));
-const variable2 = Number(prompt("Ingrese una variable: ", ));
+const variable1 = Number(prompt('Ingrese una variable: '));
+const variable2 = Number(prompt('Ingrese una variable: '));
 
-console.log(variable1);
-console.log(variable2);
+function suma(variable1, variable2) {
+    let suma = variable1 + variable2;
+    console.log(suma);
+};
+suma(variable1, variable2);
