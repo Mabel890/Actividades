ef6735a6582cb2782c03ecdcf9070bbb1590c856
Mabel <mabelvillada890@gmail.com>
2021-04-09 22:34:14 -0500

    Multiplicacion

---
+++ multiplicacion.js
@@ -0,0 +1,8 @@
+const variable1 = Number(prompt('Ingrese una variable: '));
+const variable2 = Number(prompt('Ingrese una variable: '));
+
+function multiplicacion(variable1, variable2) {
+    let multiplicacion = variable1 * variable2;
+    console.log(multiplicacion);
+};
+multiplicacion(variable1, variable2);
