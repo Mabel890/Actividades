0ed5b9db822106d5362e8bba1e19312e5d25a82f
Mabel <mabelvillada890@gmail.com>
2021-04-09 22:31:44 -0500

    Resta

---
+++ resta.js
@@ -0,0 +1,8 @@
+const variable1 = Number(prompt('Ingrese una variable: '));
+const variable2 = Number(prompt('Ingrese una variable: '));
+
+function resta(variable1, variable2) {
+    let resta = variable1 - variable2;
+    console.log(resta);
+};
+resta(variable1, variable2);
